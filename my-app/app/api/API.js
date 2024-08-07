cfc154ae396545f2c71816d8ecab6e4c41d771a6
kanade2001 <kanade2001.cpp@gmail.com>
2024-08-07 09:57:09 +0900

    修正及びVPS作成の追加

--- my-app/app/api/API.js
+++ my-app/app/api/API.js
@@ -1,6 +1,7 @@
+// トークンの取得
 const getToken = async (userid, password, projectname) => {
   try {
-    const endpoint = "https://identity.c3j1.conoha.io/v3/auth/tokens";
+    const endpoint = "https://identity.c3j1.conoha.io/v3/auth/tokens"; // APIのエンドポイント
     const response = await fetch(endpoint, {
       method: "POST",
       credentials: "include",
@@ -35,13 +36,15 @@ const getToken = async (userid, password, projectname) => {
   }
 };
 
-const getVolumeType = async (token) => {
+// ボリュームタイプの取得
+// https://doc.conoha.jp/api-vps3/volume-get_types_list-v3/
+const getVolumeType = async (token, projectname) => {
   try {
-    const endpoint = `https://block-storage.c3j1.conoha.io/v3/${process.env.PROJECTNAME}/types`;
+    const endpoint = `https://block-storage.c3j1.conoha.io/v3/${projectname}/types`;
     const response = await fetch(endpoint, {
       method: "GET",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "X-Auth-Token": token,
       },
     });
@@ -63,13 +66,16 @@ const getVolumeType = async (token) => {
   }
 };
 
+// イメージIDの取得
+// https://doc.conoha.jp/api-vps3/image-get_images_list-v3/
 const getImageID = async (token) => {
   try {
-    const endpoint = "https://image-service.c3j1.conoha.io/v2/images?name=vmi-centos-stream9-amd64";
+    const endpoint =
+      "https://image-service.c3j1.conoha.io/v2/images?name=vmi-centos-stream9-amd64";
     const response = await fetch(endpoint, {
       method: "GET",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "X-Auth-Token": token,
       },
     });
@@ -77,14 +83,124 @@ const getImageID = async (token) => {
     const decoder = new TextDecoder();
     let jsonString = "";
     while (true) {
-      const {done, value} = await reader.read();
-      if(done){
+      const { done, value } = await reader.read();
+      if (done) {
         break;
       }
-      jsonString += decoder.decode(value, {stream: true});
+      jsonString += decoder.decode(value, { stream: true });
     }
     const data = JSON.parse(jsonString);
     return data.images;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// ボリュームIDの取得
+// https://doc.conoha.jp/api-vps3/volume-create_vol-v3/
+const getVolumeID = async (token, projectname, info) => {
+  try {
+    const endpoint = `https://block-storage.c3j1.conoha.io/v3/${projectname}/volumes`;
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "X-Auth-Token": token,
+      },
+      body: JSON.stringify({ // 配列をそのままjsonにして渡したい
+        volume: {
+          size: info.size,
+          description: null,
+          name: info.name,
+          "volume-type": info.volumeType,
+          imageRef: info.imageID,
+        },
+      }),
+    });
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    let jsonString = "";
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      jsonString += decoder.decode(value, { stream: true });
+    }
+    const data = JSON.parse(jsonString);
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// フレーバーIDの取得
+// https://doc.conoha.jp/api-vps3/compute-get_flavors_list-v3/
+const getFlavorID = async (token) => {
+  console.log("FLAVOR");
+  try {
+    const endpoint = "https://compute.c3j1.conoha.io/v2.1/flavors";
+    const response = await fetch(endpoint, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "X-Auth-Token": token,
+      },
+    });
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    let jsonString = "";
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      jsonString += decoder.decode(value, { stream: true });
+    }
+    const data = JSON.parse(jsonString);
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// VPSの作成
+//  https://doc.conoha.jp/api-vps3/compute-create_vm-v3/
+const createVPS = async (token, info) => {
+  console.log("FLAVOR");
+  try {
+    const endpoint = "https://compute.c3j1.conoha.io/v2.1/servers";
+    const response = await fetch(endpoint, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "X-Auth-Token": token,
+      },
+      body: JSON.stringify({
+        server: {
+          flavorRef: info.flavorID,
+          adminPass: info.adminPass,
+          "block_device_mapping_v2":  [{"uuid": info.volumeID}],
+          "metadata": {"instance_name_tag": "test-vps"},
+          "security_groups": [{"name": "IPv4v6-SSH"}]
+        },
+      }),
+    });
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    let jsonString = "";
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      jsonString += decoder.decode(value, { stream: true });
+    }
+    const data = JSON.parse(jsonString);
+    return data;
   } catch (error) {
     console.log(error);
     throw error;
@@ -96,13 +212,25 @@ const userid = process.env.USERID;
 const password = process.env.PASSWORD;
 const projectname = process.env.PROJECTNAME;
 
-t = "";
+let serverinfo = {};
+serverinfo.size = 100;
+serverinfo.name = "nametag";
+serverinfo.volumeType = "c3j1-ds02-boot"
+serverinfo.imageID = "d2ddeb80-bcbb-417e-bb96-3e87933437e1";
+
 getToken(userid, password, projectname).then((token) => {
   console.log(token);
-  getVolumeType(token).then((data) => {
+  getVolumeType(token, projectname).then((data) => {
     console.log(data);
   });
-  getImageID(token).then(data => {
+  getImageID(token).then((data) => {
     console.log(data);
-  }) 
+    getVolumeID(token, projectname, serverinfo).then((data) => {
+      console.log(data);
+    });
+  });
+  getFlavorID(token).then((data) => {
+    console.log(data);
+  });
 });
+
