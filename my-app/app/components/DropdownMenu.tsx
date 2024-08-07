83f159e78dcde736a68ed0169b6f7232891e1a44
ren <kikuren1st@gmail.com>
2024-08-07 10:31:38 +0900

    404エラーページ作成とログインユーザーの情報をヘッダーに表示

--- my-app/app/components/DropdownMenu.tsx
+++ my-app/app/components/DropdownMenu.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { RiAccountCircleFill } from 'react-icons/ri';
 import Logout from './Logout';
+import { DropdownButton } from './icon/DropdownButton';
 
 export default function DropdownMenu() {
 
@@ -13,11 +14,30 @@ export default function DropdownMenu() {
       setIsOpen(!isOpen);
     }
 
+    const [email, setEmail] = useState<string | null>(null);
+
+    useEffect(() => {
+      const storedEmail = localStorage.getItem("userEmail");
+      setEmail(storedEmail);
+    }, []);
+
     return (
         <div>
-            <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm " id="menu-button" aria-expanded={isOpen} aria-haspopup="true" tabIndex={-1} onClick={toggleMenu}>
-                <RiAccountCircleFill className='mt-1'></RiAccountCircleFill>
-                アカウント名
+            <button
+                type="button"
+                className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm"
+                id="menu-button"
+                aria-expanded={isOpen}
+                aria-haspopup="true"
+                tabIndex={-1}
+                onClick={toggleMenu}
+            >
+                <RiAccountCircleFill className='mt-1' />
+                {email && (
+                    <p className="flex items-center">
+                        <span>{email}</span> <DropdownButton />
+                    </p>
+                )}
             </button>
 
             {isOpen && (
---
+++ my-app/app/error/404/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Custom404() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-white">
+            <h1>404 - Page Not Found</h1>
+            <p>お探しのページは見つかりませんでした。</p>
+            <br></br>
+            <Image src="/images/404-2.png" alt="404" width={120} height={120} />
+            <br></br>
+            <br></br>
+            <Link href="/dashboard" legacyBehavior>
+                <a className="text-black hover:text-blue-500">ダッシュボードに戻る</a>
+            </Link>
+        </div>
+    );
+}
