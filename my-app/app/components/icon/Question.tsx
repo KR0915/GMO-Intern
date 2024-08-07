ed7a374f6ae2645ef0fb7db8aec3c2351451c17d
ren <kikuren1st@gmail.com>
2024-08-07 14:05:37 +0900

    ヘッダーのデザインを改良した

---
+++ my-app/app/components/icon/Question.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function MynauiQuestionCircle(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"><path d="M12 13.496c0-2.003 2-1.503 2-3.506c0-2.659-4-2.659-4 0m2 6.007v-.5"></path><circle cx="12" cy="12" r="9"></circle></g></svg>
+    )
+  }
---
+++ my-app/app/components/icon/VPS.tsx
@@ -0,0 +1,8 @@
+import { SVGProps } from "react";
+
+
+export function MaterialSymbolsDataTableOutline(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zM5 8.325h14V5H5zm0 5.35h14v-3.35H5zM5 19h14v-3.325H5zM6 7.65v-2h2v2zM6 13v-2h2v2zm0 5.35v-2h2v2z"></path></svg>
+    )
+  }
