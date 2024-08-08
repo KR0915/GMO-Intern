14265d8f5bace5b51396bc4e659193b6b8a4f835
1029takuto <i.t0125@icloud.com>
2024-08-08 14:58:11 +0900

    new

--- my-app/app/components/MoneySidebar.tsx
+++ my-app/app/components/MoneySidebar.tsx
@@ -3,18 +3,21 @@ import React from 'react';
 interface MoneySidebarProps {
   plan: string | null;
   price: number | null;
+  onCreate: () => void; 
 }
 
-export default function MoneySidebar({ plan, price }: MoneySidebarProps) {
-    return (
-        <div className="ml-[1rem] border-l border-gray-400 pl-2 mt-10 bg-white sticky top-[130px] rounded-lg mx-4 p-4"> {/* ヘッダーの高さに合わせて調整 */}
-            <div className="mt-4">
-                <p>選択されたプラン: {plan || '未選択'}</p>
-                <p>価格: {price ? `${price.toLocaleString()} 円 /月` : '未選択'}</p>
-                <div className="flex justify-center items-center mt-4">
-                    <button className="bg-blue-500 text-white py-2 px-4 rounded">追加</button>
-                </div>
-            </div>
+export default function MoneySidebar({ plan, price, onCreate }: MoneySidebarProps) {
+  return (
+    <div className="ml-[1rem] border-l border-gray-400 pl-2 mt-10 bg-white sticky top-[130px] rounded-lg mx-4 p-4">
+      <div className="mt-4">
+        <p>選択されたプラン: {plan || '未選択'}</p>
+        <p>価格: {price ? `${price.toLocaleString()} 円 /月` : '未選択'}</p>
+        <div className="flex justify-center items-center mt-4">
+          <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={onCreate}>
+            追加
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
