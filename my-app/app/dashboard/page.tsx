70a36bc61d5d968867c34ad2d9dd6c305057552a
1029takuto <i.t0125@icloud.com>
2024-08-08 22:43:28 +0900

    一応コンソールには選択したものが評議されたのでこれで合ってるか確認して欲しいです

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,37 +1,29 @@
 "use client";
-import React, { useState } from 'react';
-import Service from '../components/Service';  // Serviceコンポーネントをインポート
-import Header from '../components/Header'; // Headerコンポーネントをインポート
-import Sidebar from '../components/Sidebar'; // Sidebarコンポーネントをインポート
-import MoneySidebar from '../components/MoneySidebar'; // MoneySidebarコンポーネントをインポート
-import Option from '../components/option';
+
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import Service from '../components/Service';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import MoneySidebar from '../components/MoneySidebar';
 
 export default function Home() {
-    const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
-    const [selectedPrice, setSelectedPrice] = useState<number | null>(null);
-
-    const handleSetSelectedPlan = (plan: string | null) => {
-        setSelectedPlan(plan);
-    };
-
-    const handleSetSelectedPrice = (price: number | null) => {
-        setSelectedPrice(price);
-    };
-
-    return (
-        <div className="min-h-screen flex flex-col bg-custom-bg bg-cover bg-custom-right-bottom bg-fixed overflow-y-auto">
-            <Header />
-            <div className="flex-1 grid grid-cols-5">
-                <div className="col-span-1">
-                    <Sidebar />
-                </div>
-                <div className="col-span-3">
-                    <Service setSelectedPlan={handleSetSelectedPlan} setSelectedPrice={handleSetSelectedPrice} />
-                </div>
-                <div className="col-span-1">
-                    <MoneySidebar plan={selectedPlan} price={selectedPrice} />
-                </div>
-            </div>
+  return (
+    <RecoilRoot>
+      <div className="h-screen flex flex-col bg-custom-bg bg-cover bg-custom-right-bottom bg-fixed overflow-y-auto">
+        <Header />
+        <div className="flex-1 grid grid-cols-5">
+          <div className="col-span-1">
+            <Sidebar />
+          </div>
+          <div className="col-span-3">
+            <Service />
+          </div>
+          <div className="col-span-1">
+            <MoneySidebar />
+          </div>
         </div>
-    );
+      </div>
+    </RecoilRoot>
+  );
 }
