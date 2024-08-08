1d136e404b9f7eda32e99a5ff9027d2536a934a2
ren <kikuren1st@gmail.com>
2024-08-08 16:10:53 +0900

    Service.tsxのhover時に色を変更できるようにした

--- my-app/app/components/Service.tsx
+++ my-app/app/components/Service.tsx
@@ -160,7 +160,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                 className={`bg-white border border-black shadow-md rounded-lg p-2 flex flex-col items-center text-center h-24 w-48 ${
                   selectedService === service.name 
                   ? "bg-blue-400 text-white" 
-                  : "bg-white text-black"
+                  : "bg-white text-black hover:border-blue-400 hover:text-blue-400"
                 }`}
               >
                 <h3 className="text-lg font-semibold mb-2">{service.name}</h3>
@@ -197,7 +197,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                     className={`h-20 w-full border border-black rounded text-center px-2 py-1 leading-tight ${
                       selectedAppButton === item.label
                         ? "bg-blue-400 text-white"
-                        : "bg-white text-black"
+                        : "bg-white text-black hover:border-blue-400 hover:text-blue-400"
                     }`}
                   >
                     <div className="flex items-center justify-center">
@@ -217,7 +217,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                       className={`h-20 w-full border border-black rounded text-center px-2 py-1 leading-tight ${
                         selectedAppButton === label
                           ? "bg-blue-400 text-white"
-                          : "bg-white text-black"
+                          : "bg-white text-black hover:border-blue-400 hover:text-blue-400"
                       }`}
                       dangerouslySetInnerHTML={{ __html: label }}
                     />
@@ -244,7 +244,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                       className={`rounded px-4 py-2 ${
                         selectedPricing === option
                           ? "bg-blue-400 text-white"
-                          : "bg-white border border-black text-black"
+                          : "bg-white border border-black text-black hover:border-blue-400 hover:text-blue-400"
                       }`}
                     >
                       {option}
@@ -263,7 +263,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                       className={`rounded px-4 py-2 ${
                         selectedPlanLocal === plan.size
                           ? "bg-blue-400 text-white"
-                          : "bg-white border border-black text-black"
+                          : "bg-white border border-black text-black hover:border-blue-400 hover:text-blue-400"
                       }`}
                     >
                       <div className="text-center">
@@ -290,7 +290,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                     className={`h-24 w-36 rounded-full border border-black text-center px-4 py-2 leading-tight ${
                       selectedOSButton === label
                         ? "bg-blue-300 text-white"
-                        : "bg-white text-black"
+                        : "bg-white text-black hover:border-blue-400 hover:text-blue-400"
                     }`}
                   >
                     {label}
@@ -307,7 +307,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                       className={`rounded px-4 py-2 ${
                         selectedPricing === option
                           ? "bg-blue-400 text-white"
-                          : "bg-white border border-black text-black"
+                          : "bg-white border border-black text-black hover:border-blue-400 hover:text-blue-400"
                       }`}
                     >
                       {option}
@@ -325,7 +325,7 @@ export default function Services({ setSelectedPlan, setSelectedPrice }: ServiceP
                       className={`rounded px-4 py-2 ${
                         selectedPlanLocal === plan.size
                           ? "bg-blue-400 text-white"
-                          : "bg-white border border-black text-black"
+                          : "bg-white border border-black text-black hover:border-blue-400 hover:text-blue-400"
                       }`}
                     >
                       <div className="text-center">
