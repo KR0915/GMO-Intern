95025b08a8bc909f5e494be46a1eea2602465336
kanade2001 <kanade2001.cpp@gmail.com>
2024-08-08 19:34:49 +0900

    APItest単体で動作

--- my-app/app/api/getimageid/route.ts
+++ my-app/app/api/getimageid/route.ts
@@ -1,9 +1,17 @@
 // イメージIDの取得
 // https://doc.conoha.jp/api-vps3/image-get_images_list-v3/
+
+type JsonObj = {
+  id: string;
+  tags: string[];
+  name: string;
+};
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const user = searchParams.get("user");
+    const target = searchParams.get("target");
     const token = process.env.NEXT_PUBLIC_TOKEN as string;
 
     const endpoint = "https://image-service.c3j1.conoha.io/v2/images?limit=200";
@@ -25,7 +33,19 @@ export async function GET(request: Request) {
       jsonString += decoder.decode(value, { stream: true });
     }
     const data = JSON.parse(jsonString);
-    return Response.json(data.images);
+    if (target != null) {
+      var targetID = "Hello";
+      data.images.forEach((item: JsonObj) => {
+        item.tags.forEach((tag: string) => {
+          if (tag === `app_name=${target}`) {
+            targetID = item.id;
+          }
+        });
+      });
+      return Response.json(targetID);
+    } else {
+      return Response.json(data.images);
+    }
   } catch (error) {
     console.log(error);
     throw error;
